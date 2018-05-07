5f75f298b72aa1ebaa25072cb3bdd2fdbee5b97c
brasilia <ksuspec@gmail.com>
2018-05-06 22:53:06 -0300

    Acho que o git add fez falta sim, tava faltando algumas coisas

---
+++ Assets/Scripts/GameObjects/CardTable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardTable : MonoBehaviour {
+
+	public List<Button> btnCandidate;
+	private GameManager gameManager;
+	private Player player;
+	public int candidateSelected;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameManager.instance;
+		//player = GameObject.Find ("Player").GetComponent<Player> ();
+		candidateSelected = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Ativa o widget.
+	public void SetActiveSelectScreen(List<GameObject> candidates){
+		Debug.Log (candidates.Count);
+		gameObject.SetActive (true);
+		int i;
+		for (i = 0; i < candidates.Count; i++) {
+			Debug.Log (i + " " + candidates [i]);
+			candidates [i].transform.SetParent (btnCandidate [i].transform);
+			candidates [i].transform.position = btnCandidate [i].transform.position;
+		}
+	}
+
+	// Identifica o candidato escolhido.
+	public void OnClickCandidate(int candidateSelected){
+		this.candidateSelected = candidateSelected;
+		//print ("Você selecionou o candidato " + gameManager.otherCandidates [candidateSelected].name);
+		//if (gameManager.State == GameManager.STATE.ChooseCandidate)
+		//gameManager.CandidateChosen (candidateSelected);
+		gameManager.ReturnControl();
+	}
+		
+}
