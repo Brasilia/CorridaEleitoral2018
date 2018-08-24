a7bb79b06373691c7be011df740e990f4446bcc2
fcontinitorres <fcontinitorres@gmail.com>
2018-08-23 21:42:08 -0300

    criacao de nova cena teste para grafico com script e lineRenders

--- Assets/Scripts/GraphControl.cs
+++ Assets/Scripts/GraphControl.cs
@@ -7,42 +7,75 @@ public class GraphControl : MonoBehaviour {
 	public GameObject graph;		 //referencia para o grafico
 	public LineRenderer baseX;		 //eixo x
 	public LineRenderer baseY;		 //eixo y
-	public List<Player> candidates;	 //lista dos candidatos
 	public GameObject lines;		 //referencia p/ prefab para emptyObject com LineRender
 
+    public GameManager gm;
 
-	// Use this for initialization
-	void Start () {
-		//baseX = baseX.gameObject.GetComponent<LineRenderer> ();
-		Vector3[] positions = new Vector3[2];
+    private float panelWidth;
+    private float panelHeight;
 
-		positions [0] = new Vector3 (0f, 0f, 0f);
-		positions [1] = new Vector3 (20.0f, 0.0f, 0.0f);
+
+    // Use this for initialization
+    void Start () {
+
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+        //baseX = baseX.gameObject.GetComponent<LineRenderer> ();
+        Vector3[] positions = new Vector3[2];
+
+        RectTransform panel = (RectTransform) graph.transform;
+        panelWidth = panel.rect.width;
+        panelHeight = panel.rect.height;
+
+        positions [0] = new Vector3 (0f, 0f, -1f);
+		positions [1] = new Vector3 (panelWidth, 0.0f, -1f);
 		baseX.positionCount = positions.Length;
-		Material m = baseX.GetComponent<Renderer> ().material;
+        baseX.SetPositions(positions);
+        //Material m = baseX.GetComponent<Renderer> ().material;
+        //baseX.material = new Material(Shader.Find("Particles/Additive")).SetColors(Color.white, Color.black);
 
-		baseX.SetPositions (positions);
-
-		positions [0] = new Vector3 (0f, 0f, 0f);
-		positions [1] = new Vector3 (0f, 5f, 0f);
+        positions[0] = new Vector3 (0f, 0f, -1f);
+		positions [1] = new Vector3 (0f, panelHeight, 0f);
 		baseY.positionCount = positions.Length;
 		baseY.SetPositions (positions);
 
-		foreach (Player cand in candidates) {
-			GameObject inst = Instantiate (lines);
-			inst.transform.parent = graph.transform;
-			inst.transform.localPosition = new Vector3 (0f, 0f, 0f);
-			LineRenderer line = inst.GetComponent<LineRenderer> ();
-			positions [0] = new Vector3 (0f, 0f, 0f);
-			positions [1] = new Vector3 (10f, 5f, 0f);
-			line.positionCount = positions.Length;
-			line.useWorldSpace = false;
-			line.SetPositions (positions);
-		}
-	}
+
+        List<float> votIntentions = new List<float>();
+        votIntentions.Add(0.5f);
+        votIntentions.Add(0.3f);
+        votIntentions.Add(0.7f);
+
+        Vector3[] positionGraph = new Vector3[4];
+        positionGraph[0] = new Vector3(0f, 0f, -1f);
+        int i = 1;
+        foreach (float f in votIntentions)
+        {
+            print("AQUI: " + 0.1f * panelWidth + " " + f * panelHeight);
+            positionGraph[i] = new Vector3((0.1f * panelWidth * (i+1)), (f * panelHeight), -1f);
+            i++;
+        }
+
+        GameObject inst = Instantiate(lines);
+        //add instancia como filho do grafico
+        inst.transform.parent = graph.transform;
+        inst.transform.localPosition = new Vector3(0f, 0f, 0f);
+        inst.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        LineRenderer line = inst.GetComponent<LineRenderer>();
+        line.positionCount = positionGraph.Length;
+        line.SetPositions(positionGraph);
+
+        foreach (Candidate cand in gm.candidates) {
+            //positions [1] = new Vector3 ((0.1f * panelWidth), (cand.voteIntentions * panelHeight), -1f);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void SetGraphActive() {
+        gameObject.SetActive(true);
+    }
 }
