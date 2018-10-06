db62deacf02a9e64f99bb5579663b158aa9ea40a
brasilia <ksuspec@gmail.com>
2018-10-06 17:43:42 -0300

    Imagem automática em cartas

---
+++ Assets/AutoCardImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoCardImage : MonoBehaviour {
+
+	private Image cardImage;
+	private GameManager gm;
+
+	void Awake () {
+		cardImage = GetComponent<Image> ();
+		gm = GameManager.instance;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable () {
+		if (cardImage.sprite == null) {
+			if ((int)gm.state >= (int)GameManager.STATE.DebateQuestion) {
+				cardImage.sprite = gm.candidates [gm.opponentIndex].image;
+			}
+		}
+	}
+}
